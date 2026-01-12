8c7b24b978d27796abde460cb5cef6940069300b
lyacolem-prog <lyacolem@ucsc.edu>
2026-01-12 11:19:23 -0800

    push lynx.py Solution for Code Journal 1 prompt 1

---
+++ lynx.py
@@ -0,0 +1,6 @@
+# Lynx Coleman - Jan 7th 2025 
+
+print("Lynx Coleman he/they")
+print("I love Salmon, usally seared with a bit of butter and spice and My Favorite movie was the Boy and the Heron, Surrealism is nice.")
+
+
